fd3570e9bf953370a6f2f214f5d31dec1f357859
rehmanm <muhammad.abdulrehman@gmail.com>
2024-05-10 16:14:41 -0400

    vrf

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.23;
-
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 /**
  * title A sample Raffle Contract
  * @author Muhammad Abdul Rehman (following cyfrin foundry 101 - Patrick Collins)
@@ -9,19 +10,39 @@ pragma solidity ^0.8.23;
  * @dev Implements Chainlink VRFV2
  */
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughEthSent();
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+
+    VRFCoordinatorV2Interface private immutable i_vrfCooridinator;
+    bytes32 private immutable i_gaslane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCooridinator,
+        bytes32 gaslane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCooridinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
+        i_vrfCooridinator = VRFCoordinatorV2Interface(vrfCooridinator);
+        i_gaslane = gaslane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
         s_lastTimeStamp = block.timestamp;
     }
 
@@ -37,12 +58,30 @@ contract Raffle {
 
     // 1. Get a random number
     // 2. Pick a winner
-    function pickWinner() external view {
+    function pickWinner() external {
         //check to see if enough time has
 
         if (block.timestamp - s_lastTimeStamp < i_interval) {
             revert();
         }
+
+        uint256 requestId = i_vrfCooridinator.requestRandomWords(
+            i_gaslane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        // require(s_requests[_requestId].exists, "request not found");
+        // s_requests[_requestId].fulfilled = true;
+        // s_requests[_requestId].randomWords = _randomWords;
+        // emit RequestFulfilled(_requestId, _randomWords);
     }
 
     /** Getter Functions */
